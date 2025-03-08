cd46a8172f963b7fa9506f8401734f3b6f0c26b4
tbengu <tbengu@bhengubv.com>
2025-03-08 16:09:00 +0200

    Regenerate App.xaml.cs with CreateWindow for MAUI app

--- OpenISP/OpenISP/App.xaml.cs
+++ OpenISP/OpenISP/App.xaml.cs
@@ -1,14 +1,17 @@
 ﻿using Microsoft.Maui.Controls;
 
-namespace OpenISP
-{
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
+namespace OpenISP;
 
-            MainPage = new MainPage(); // Assuming MainPage hosts the BlazorWebView
-        }
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        // Create a new Window with MainPage as the root page
+        return new Window(new MainPage());
     }
 }
